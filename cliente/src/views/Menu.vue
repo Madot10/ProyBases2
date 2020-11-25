9096a55a9b1aa26fcdf7786a925bfbc307857d9b
Madot <migueldeolim1@gmail.com>
2020-11-24 21:07:08 -0400

    Interfaz de Menu principal

---
+++ cliente/src/views/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-container>
+        <b-row class="vh-100" align-v="center">
+            <b-col col></b-col>
+            <b-col col>
+                <BtnMenu title="REPORTES" nameUrl="Reportes"></BtnMenu>
+                <BtnMenu title="SIMULACIÓN" nameUrl="Simulacion"></BtnMenu>
+            </b-col>
+            <b-col col></b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import BtnMenu from "../components/BtnMenu.vue";
+
+export default {
+    components: { BtnMenu },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style>
+body {
+    background-image: url("~@/assets/fondo_menu.png");
+
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: 40% 0;
+}
+</style>

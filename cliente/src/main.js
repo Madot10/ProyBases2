4d0f68045e93fc5a82f90ca7aaca3a1d344d5e0a
Madot <migueldeolim1@gmail.com>
2020-11-28 14:48:53 -0400

    Actualizando e incluyendo paquete de iconos

--- cliente/src/main.js
+++ cliente/src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import { IconsPlugin } from "bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(IconsPlugin);
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
